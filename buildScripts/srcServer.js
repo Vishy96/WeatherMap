38bfd1d6767e5da648a9a155ebe5674c4de25ce7
Vishy96 <viseslav.aracic@fer.hr>
2017-08-14 09:45:10 +0200

    Initial commit

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,49 @@
+/* import express from 'express'
+import path from 'path'
+import open from 'open' */
+
+/*
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+var app = express();
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+
+})
+
+app.listen(port, function(error){
+    if(error)
+        console.log(error);
+    else {
+        open('http://localhost:' + port);
+    }
+
+})
+ */
+
+ var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+var app = express();
+
+/*app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+
+})
+*/
+
+
+app.listen(port, function(error){
+    if(error)
+        console.log(error);
+    else {
+        open('http://localhost:' + port);
+    }
+
+})
---
+++ src/index.js
@@ -0,0 +1,626 @@
+var weatherIconsFile = {
+  "200": {
+    "label": "thunderstorm with light rain",
+    "icon": "storm-showers"
+  },
+
+  "201": {
+    "label": "thunderstorm with rain",
+    "icon": "storm-showers"
+  },
+
+  "202": {
+    "label": "thunderstorm with heavy rain",
+    "icon": "storm-showers"
+  },
+
+  "210": {
+    "label": "light thunderstorm",
+    "icon": "storm-showers"
+  },
+
+  "211": {
+    "label": "thunderstorm",
+    "icon": "thunderstorm"
+  },
+
+  "212": {
+    "label": "heavy thunderstorm",
+    "icon": "thunderstorm"
+  },
+
+  "221": {
+    "label": "ragged thunderstorm",
+    "icon": "thunderstorm"
+  },
+
+  "230": {
+    "label": "thunderstorm with light drizzle",
+    "icon": "storm-showers"
+  },
+
+  "231": {
+    "label": "thunderstorm with drizzle",
+    "icon": "storm-showers"
+  },
+
+  "232": {
+    "label": "thunderstorm with heavy drizzle",
+    "icon": "storm-showers"
+  },
+
+  "300": {
+    "label": "light intensity drizzle",
+    "icon": "sprinkle"
+  },
+
+  "301": {
+    "label": "drizzle",
+    "icon": "sprinkle"
+  },
+
+  "302": {
+    "label": "heavy intensity drizzle",
+    "icon": "sprinkle"
+  },
+
+  "310": {
+    "label": "light intensity drizzle rain",
+    "icon": "sprinkle"
+  },
+
+  "311": {
+    "label": "drizzle rain",
+    "icon": "sprinkle"
+  },
+
+  "312": {
+    "label": "heavy intensity drizzle rain",
+    "icon": "sprinkle"
+  },
+
+  "313": {
+    "label": "shower rain and drizzle",
+    "icon": "sprinkle"
+  },
+
+  "314": {
+    "label": "heavy shower rain and drizzle",
+    "icon": "sprinkle"
+  },
+
+  "321": {
+    "label": "shower drizzle",
+    "icon": "sprinkle"
+  },
+
+  "500": {
+    "label": "light rain",
+    "icon": "rain"
+  },
+
+  "501": {
+    "label": "moderate rain",
+    "icon": "rain"
+  },
+
+  "502": {
+    "label": "heavy intensity rain",
+    "icon": "rain"
+  },
+
+  "503": {
+    "label": "very heavy rain",
+    "icon": "rain"
+  },
+
+  "504": {
+    "label": "extreme rain",
+    "icon": "rain"
+  },
+
+  "511": {
+    "label": "freezing rain",
+    "icon": "rain-mix"
+  },
+
+  "520": {
+    "label": "light intensity shower rain",
+    "icon": "showers"
+  },
+
+  "521": {
+    "label": "shower rain",
+    "icon": "showers"
+  },
+
+  "522": {
+    "label": "heavy intensity shower rain",
+    "icon": "showers"
+  },
+
+  "531": {
+    "label": "ragged shower rain",
+    "icon": "showers"
+  },
+
+  "600": {
+    "label": "light snow",
+    "icon": "snow"
+  },
+
+  "601": {
+    "label": "snow",
+    "icon": "snow"
+  },
+
+  "602": {
+    "label": "heavy snow",
+    "icon": "snow"
+  },
+
+  "611": {
+    "label": "sleet",
+    "icon": "sleet"
+  },
+
+  "612": {
+    "label": "shower sleet",
+    "icon": "sleet"
+  },
+
+  "615": {
+    "label": "light rain and snow",
+    "icon": "rain-mix"
+  },
+
+  "616": {
+    "label": "rain and snow",
+    "icon": "rain-mix"
+  },
+
+  "620": {
+    "label": "light shower snow",
+    "icon": "rain-mix"
+  },
+
+  "621": {
+    "label": "shower snow",
+    "icon": "rain-mix"
+  },
+
+  "622": {
+    "label": "heavy shower snow",
+    "icon": "rain-mix"
+  },
+
+  "701": {
+    "label": "mist",
+    "icon": "sprinkle"
+  },
+
+  "711": {
+    "label": "smoke",
+    "icon": "smoke"
+  },
+
+  "721": {
+    "label": "haze",
+    "icon": "day-haze"
+  },
+
+  "731": {
+    "label": "sand, dust whirls",
+    "icon": "cloudy-gusts"
+  },
+
+  "741": {
+    "label": "fog",
+    "icon": "fog"
+  },
+
+  "751": {
+    "label": "sand",
+    "icon": "cloudy-gusts"
+  },
+
+  "761": {
+    "label": "dust",
+    "icon": "dust"
+  },
+
+  "762": {
+    "label": "volcanic ash",
+    "icon": "smog"
+  },
+
+  "771": {
+    "label": "squalls",
+    "icon": "day-windy"
+  },
+
+  "781": {
+    "label": "tornado",
+    "icon": "tornado"
+  },
+
+  "800": {
+    "label": "clear sky",
+    "icon": "sunny"
+  },
+
+  "801": {
+    "label": "few clouds",
+    "icon": "cloudy"
+  },
+
+  "802": {
+    "label": "scattered clouds",
+    "icon": "cloudy"
+  },
+
+  "803": {
+    "label": "broken clouds",
+    "icon": "cloudy"
+  },
+
+  "804": {
+    "label": "overcast clouds",
+    "icon": "cloudy"
+  },
+
+
+  "900": {
+    "label": "tornado",
+    "icon": "tornado"
+  },
+
+  "901": {
+    "label": "tropical storm",
+    "icon": "hurricane"
+  },
+
+  "902": {
+    "label": "hurricane",
+    "icon": "hurricane"
+  },
+
+  "903": {
+    "label": "cold",
+    "icon": "snowflake-cold"
+  },
+
+  "904": {
+    "label": "hot",
+    "icon": "hot"
+  },
+
+  "905": {
+    "label": "windy",
+    "icon": "windy"
+  },
+
+  "906": {
+    "label": "hail",
+    "icon": "hail"
+  },
+
+  "951": {
+    "label": "calm",
+    "icon": "sunny"
+  },
+
+  "952": {
+    "label": "light breeze",
+    "icon": "cloudy-gusts"
+  },
+
+  "953": {
+    "label": "gentle breeze",
+    "icon": "cloudy-gusts"
+  },
+
+  "954": {
+    "label": "moderate breeze",
+    "icon": "cloudy-gusts"
+  },
+
+  "955": {
+    "label": "fresh breeze",
+    "icon": "cloudy-gusts"
+  },
+
+  "956": {
+    "label": "strong breeze",
+    "icon": "cloudy-gusts"
+  },
+
+  "957": {
+    "label": "high wind, near gale",
+    "icon": "cloudy-gusts"
+  },
+
+  "958": {
+    "label": "gale",
+    "icon": "cloudy-gusts"
+  },
+
+  "959": {
+    "label": "severe gale",
+    "icon": "cloudy-gusts"
+  },
+
+  "960": {
+    "label": "storm",
+    "icon": "thunderstorm"
+  },
+
+  "961": {
+    "label": "violent storm",
+    "icon": "thunderstorm"
+  },
+
+  "962": {
+    "label": "hurricane",
+    "icon": "cloudy-gusts"
+  }
+};
+var map = L.map('map'); 
+const DEFAULT_ZOOM = 7;
+var currentLayer;       
+var currentZoomLevel = DEFAULT_ZOOM; 
+var currentWeatherIcons = [];    
+//map.on('load', onMapLoad);                TODO: shvatiti zasto ovako ne mogu rijesti onMapLoad() funkciju, da izbjegnem pisanje na dva mjesta                                
+var geoid = navigator.geolocation.getCurrentPosition(geo_success, geo_error, geo_options); 
+
+map.on("moveend", function (e) { re_callWeatherIcons(); });
+
+function geo_success(position) {
+ 
+ mapInit(position.coords.latitude, position.coords.longitude, DEFAULT_ZOOM);
+ onMapLoad();
+}
+
+function geo_error() {
+      const MAP_REQUEST_API_KEY = "hwG85epZUf05bOLoXpgqgte8Ga0qnejp";
+      //zatrazi input od strane korisnika te inicijaliziraj mapu
+      //tako sto ces zatraziti od Map Request API city/street latLng Object, te tako
+      //postaviti pocetni lat i lon
+
+      
+      
+      // stvori input elemente preko kojih primam user input 
+      let user_input = document.createElement('input');
+      user_input.type = "text";
+      user_input.id = "userInput";
+      let button_submit = document.createElement('button');
+      button_submit.textContent = 'Submit';
+      document.body.appendChild(user_input);
+      document.body.appendChild(button_submit);
+
+      button_submit.addEventListener("click", function Submitter (){
+              manualMapSet(user_input.value, MAP_REQUEST_API_KEY, DEFAULT_ZOOM);
+              document.body.removeChild(user_input);
+              document.body.removeChild(button_submit);
+
+      }); 
+  };
+  
+
+var geo_options = {
+  enableHighAccuracy: false, 
+  maximumAge        : 30000, 
+  timeout           : 30000
+};
+ 
+function mapInit(latitude, longitude, default_zoom){
+
+  map.setView([latitude, longitude], default_zoom);  
+
+  // leaflet search map plugin. Pretrazivanje pomocu web API nominatim.openstreetmap.org. Podrazumijeva se da postoji  
+  // varijabla 'map' koja je leaflet object L.map
+  map.addControl( new L.Control.Search({
+		url: 'http://nominatim.openstreetmap.org/search?format=json&q={s}',
+		jsonpParam: 'json_callback',
+		propertyName: 'display_name',
+		propertyLoc: ['lat','lon'],
+		marker: L.circleMarker([0,0],{radius:30}),
+		autoCollapse: true,
+		autoType: false,
+		minLength: 2
+    }));
+}
+
+//TODO: implementirati nekakav View element pomocu kojeg user radi switch
+function mapSet(map_provider){
+    map_provider = 'Google';
+    switch (map_provider) {
+    case 'Bing':
+      currentLayer = createBingLayer();
+      break;
+    case 'Google':
+      currentLayer = createGoogleLayer();
+        break;
+    default:
+      currentLayer = createOSMLayer();
+  }   
+   setCurrentLayer();  
+};
+
+
+function createOSMLayer(){
+ let osmLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoidmlzZXNsYXYiLCJhIjoiY2o2NHVvOWUwMXZ5YTJ3anhkamZtMmV4dyJ9.fCnexghPYMIuM29dTcfvSA',
+ 
+     });
+
+  return osmLayer;
+}
+
+
+function createBingLayer(){
+  const BING_KEY ='AiqfCwhUnUy97WyEF547k75oSgoRk8SGFYBfCNZRi7zJzurEv0pWOSU6B6LZxz2s';
+  let bingLayer = new L.BingLayer(BING_KEY, {type: 'Road'});
+  return bingLayer;
+
+}
+
+function createGoogleLayer(){
+  const GOOGLE_MAPS_API_SRC = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCPBQctYUskPMMYeEDpEEPEdvSH5z8MpAY";
+  let googleLayer;
+  //dinamicki dodaj script za google API ukoliko korisnik odluci koristiti Google Maps 
+  let script = document.createElement('script');
+  script.src = GOOGLE_MAPS_API_SRC;
+  document.body.appendChild(script);
+
+  googleLayer = L.gridLayer.googleMutant({
+      type: 'roadmap' // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
+  });
+    return googleLayer;
+}
+
+function setCurrentLayer (){
+  currentLayer.addTo(map);
+}
+
+
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status);
+      }
+    };
+    xhr.send();
+};
+
+
+//manualMapSet je funkcija iz koje pozivam 'map request' API kako bi vratila kordinate trazenog grada
+
+ function manualMapSet (cityName, MAP_REQUEST_API_KEY, DEFAULT_ZOOM){ 
+
+            let url_constructor_MR = 'http://www.mapquestapi.com/geocoding/v1/address?key=' + MAP_REQUEST_API_KEY + '&location=' + cityName;
+          
+            getJSON(url_constructor_MR, function(err, data) {
+              
+            if (err != null) {
+              default_lat = 45.813155;  //Zagreb
+              default_lng = 15.97703;
+              mapInit(default_lat, default_lng, DEFAULT_ZOOM);  
+              alert('Something went wrong: ' + err + ' Setting default...');
+
+            } else {
+              mapInit(data.results[0].locations[0].latLng.lat, data.results[0].locations[0].latLng.lng, DEFAULT_ZOOM);
+            }
+             onMapLoad();  //set map current layer, TO DO: ukoliko uspijem osposobiti onload event listener, mogao bih onMapLoad premjestiti tamo
+                        
+              });
+  };
+        
+  function getOWMparam (){
+    let mapParams = {
+      lon_left : map.getBounds().getWest(),
+      lon_right: map.getBounds().getEast(),
+      lat_bottom: map.getBounds().getSouth(),
+      lat_top: map.getBounds().getNorth(),
+      zoom_level: map.getZoom()
+    }  
+    return mapParams;
+  };
+
+  //workflow funkcije callWeatherIcons: callWeatherIcons() -> getOWMparam() -> getJSON(url, handleOWM) -> handleOWM() -> setWeatherIcons()
+var callWeatherIcons = function (){
+      const OWM_API_KEY = '0826bd98905da40265152b2bb7f9d3e8';
+      let owmParams = getOWMparam();
+      let url_constructor_OWM = 'http://api.openweathermap.org/data/2.5/box/city?bbox=' + owmParams.lon_left + ','
+                                                                                        + owmParams.lat_bottom + ','
+                                                                                        + owmParams.lon_right + ','
+                                                                                        + owmParams.lat_top + ','
+                                                                                        + owmParams.zoom_level 
+                                                                                        + '&APPID=' 
+                                                                                        + OWM_API_KEY 
+      //poziv na API OWM                                                                                  + '&units=metric';
+      getJSON(url_constructor_OWM, handleOWM);    
+                 
+  };
+  
+    //callback funkcija: ima pristup OWM objectu koji sadrzi sve gradove koji su trenutno na mapi
+var handleOWM = function (err, owmObject){
+
+          if (err != null) {
+              alert("Error occured while trying to reach data from OWM API!");
+            } else {  
+              setWeatherIcons(owmObject);
+
+  }
+  };
+
+var setWeatherIcons = function (owmObjectReference){
+
+              const prefix = 'wi wi-';
+              
+              let code;
+              let icon;
+
+              for(let i = 0; i < owmObjectReference.cnt; i++){
+              code = owmObjectReference.list[i].weather[0].id;
+              icon = weatherIconsFile[code].icon;
+           
+              //PLUGIN CODE: ()
+              // If we are not in the ranges mentioned above, add a day/night prefix.
+              if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+                icon = 'day-' + icon;
+              }
+             // Finally tack on the prefix.
+              icon = prefix + icon;
+              //PLUGIN CODE END
+              var myIcon = [];
+              myIcon[i] = L.divIcon({className: icon,
+                           iconSize: [-20,10]  //pomak od prave lokacije na mapi
+                            });
+                   
+                
+                  let string_title = ["City: ", "Weather: "];
+                  let string_popup = [];   
+                  string_popup[i]  = '         <b>City:</b> ' + owmObjectReference.list[i].name + '<br>' +
+                                     '<b>Temperature (°C):</b> ' + owmObjectReference.list[i].main.temp + '<br>' +
+                                     '    <b>Humidity (%):</b> ' + owmObjectReference.list[i].main.humidity + '<br>' +
+                                     '  <b>Pressure (hPa):</b> ' + owmObjectReference.list[i].main.pressure + '<br>' +
+                                     '<b>Wind speed (m/s):</b> ' + owmObjectReference.list[i].wind.speed;
+                    
+                      
+                  
+
+                  currentWeatherIcons[i] = L.marker([owmObjectReference.list[i].coord.Lat, owmObjectReference.list[i].coord.Lon], {icon: myIcon[i],
+                                                                                     title: string_title[0] + owmObjectReference.list[i].name 
+                                                                                    + '\n' +  string_title[1] + owmObjectReference.list[i].weather[0].description}).bindPopup(string_popup[i]).addTo(map);
+
+                             
+              }
+                               
+                };
+
+  var onMapLoad = function(){
+  
+    mapSet();  //postavi current map layer
+    callWeatherIcons();
+  }
+
+var re_callWeatherIcons = function () {
+    clearWeatherIcons();          //TODO: osiguraj sa promisima
+    callWeatherIcons();
+};
+
+var clearWeatherIcons = function () {
+  //nazalost, clear/set pomocu Clustera (efikasnost++), iz nekog razloga, ne radi kao sto je navedeno u dokumentaciji -> prisiljen korisitit for petlju
+    for(let i=0; i<currentWeatherIcons.length ; i++)
+      map.removeLayer(currentWeatherIcons[i]);
+};
